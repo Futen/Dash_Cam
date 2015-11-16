a270ace59e1fd88c4c2cb64732a66ce56d8e38f0
Futen <tdk356@yahoo.com.tw>
2015-11-17 03:08:35 +0800

    fix bug

--- ExtractFrame.py
+++ ExtractFrame.py
@@ -7,4 +7,5 @@ for line in open('video_1_to_2_minute.txt','r'):
     line = line.split("\n")[0]
     dir_name = line.split("\t")[0] 
     video_name = dir_name + '.mp4'
-    call("ffmpeg -i Videos/%s/%s -r 5 -qscale:v 1 Videos/%s/images/image-%5d.jpg"%(dir_name, video_name, dir_name), shell=True)
+    command = "ffmpeg -i Videos/%s/%s -r 5 -qscale:v 1 Videos/%s/images/"%(dir_name, video_name, dir_name) + "image-%5d.jpg"
+    call(command, shell=True)
