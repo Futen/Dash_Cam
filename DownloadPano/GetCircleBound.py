5734d1d94767ddc4bb256f95c23a43b492b4088a
Futen <tdk356@yahoo.com.tw>
2015-11-27 03:48:45 +0800

    circle

---
+++ DownloadPano/GetCircleBound.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+
+import sys
+sys.path.append('/home/Futen/Dash_Cam')
+from geopy.distance import VincentyDistance
+import numpy as np
+import SystemParameter
+
+step_latlon = 0.0009
+step_ten_meter = step_latlon/10
+RADIUS = SystemParameter.RADIUS #km
+
+def GetBoundCircle(center): # lat, lon
+    circle = []
+    last_val = 0
+    shutdown = 0
+    add_val = step_latlon
+    difference = 0
+    while shutdown == 0: # lon
+        dis = VincentyDistance(center, (center[0], center[1]+add_val)).km
+        if dis >= RADIUS:
+            difference = add_val
+            shutdown = 1
+        add_val += step_latlon
+    bound_lat = (center[0]-difference, center[0]+difference)
+    bound_lon = (center[1]-difference, center[1]+difference)
+    bound_lat = np.arange(bound_lat[0], bound_lat[1], step_ten_meter)
+    bound_lon = np.arange(bound_lon[0], bound_lon[1], step_ten_meter)
+    #print bound_lat 
+    #print bound_lon
+    for lat in bound_lat:
+        for lon in bound_lon:
+            if VincentyDistance(center, (lat,lon)).km <= 3:
+                latlon = [lat,lon]
+                circle.append(latlon)
+    circle = np.array(circle)
+    return circle
+
+if __name__ == '__main__':
+    argv = []
+    #print len(sys.argv)-1
+    for i in range(1, len(sys.argv)):
+        argv.append(float(sys.argv[i]))
+    print VincentyDistance((argv[0], argv[1]), (argv[2], argv[3])).km
--- SystemParameter.py
+++ SystemParameter.py
@@ -1,3 +1,6 @@
 # Global Parameter
 ROOT_PATH = '/home/Futen/Dash_Cam' # root directory
 VIDEO_PATH = '/home/Futen/Dash_Cam/Videos' # Location of video
+
+# parameter for pano
+RADIUS = 3 # download pano with RADIUS km in circle
