bc62cc0cf8f6653ec9a38382fe44da7653d0a839
Futen <tdk356@yahoo.com.tw>
2015-12-04 16:03:01 +0800

    gg

--- SystemParameter.py
+++ SystemParameter.py
@@ -6,21 +6,24 @@ VIDEO_PATH = '/home/Futen/Dash_Cam/Videos' # Location of video
 
 # parameter for pano
 RADIUS = 1 # download pano with RADIUS km in circle
+FRAME_PATH = 'images' # Videos/vname/images
 PANO_PATH = 'pano' # Videos/'vname'/pano ------> VIDEO_PATH + vname + PANO_PATH
 PANO_UNCUT_PATH = '%s/download'%PANO_PATH # Video/'vname'/pano/download
 PANO_CUT_PATH = '%s/cut' %PANO_PATH # Video/'vname'/pano/cut
 
 
+# Do not change the following code 
 # The function is to get the related folder of video
-def GetPanoPath(video_name): #GetPath('ZCTXXXX')
+def GetPath(video_name): #GetPath('ZCTXXXX')
     video_path = VIDEO_PATH + '/' + video_name
     if not(os.path.isdir(video_path)) or '\n' in video_name:
         print 'error video dir or /n in name'
         exit()
+    frame_path = video_path + '/' + FRAME_PATH
     pano_path = video_path + '/' + PANO_PATH
     pano_uncut_path = video_path + '/' + PANO_UNCUT_PATH
     pano_cut_path = video_path + '/' + PANO_CUT_PATH
-    output = dict({'video_path':video_path, 'pano_path':pano_path, 'pano_uncut_path':pano_uncut_path, 'pano_cut_path':pano_cut_path})
+    output = dict({'video_path':video_path,'frame_path':frame_path, 'pano_path':pano_path, 'pano_uncut_path':pano_uncut_path, 'pano_cut_path':pano_cut_path})
     for index, key in enumerate(output):
         if not(os.path.isdir(output[key])):
             subprocess.call('mkdir -p %s'%output[key], shell=True)
